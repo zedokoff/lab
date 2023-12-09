49a4e387e1d6436b0ba382b2e6bfabb952f831b1
zedokoff <149219067+zedokoff@users.noreply.github.com>
2023-12-09 14:01:58 +1000

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+Tutorial
+$ export GITHUB_USERNAME=<имя_пользователя>
+$ export GITHUB_EMAIL=<адрес_почтового_ящика>
+$ export GITHUB_TOKEN=<сгенирированный_токен>
+$ alias edit=<nano|vi|vim|subl>
+$ cd ${GITHUB_USERNAME}/workspace
+$ source scripts/activate
+$ mkdir ~/.config
+$ cat > ~/.config/hub <<EOF
